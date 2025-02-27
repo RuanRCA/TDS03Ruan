292c71479a6b3950aa882b651c4c274135a7ef8e
RuanRCA <assuncaoruan7@gmail.com>
2025-02-27 01:17:33 -0300

    selectupdate

--- HTML e CSS/javascript/Semana.js
+++ HTML e CSS/javascript/Semana.js
@@ -1,4 +1,46 @@
-function checar(){
-    let check = window.document('check');
+function verificacao(){
+    let select = window.document.getElementById('select');
+    let resultado = String(select.value);
+    let mostrar = window.document.getElementById('mostrar');
+
+    switch(resultado){
+
+        case '0':
+        mostrar.innerHTML = '<strong> Domingo </strong>';
+
+        break
+
+        case '1':
+        mostrar.innerHTML = '<strong> Segunda-Feira </strong>';
+
+        break
+
+        case '2':
+        mostrar.innerHTML = '<strong> Terça-Feira </strong>';
+
+        break
+
+        case '3':
+        mostrar.innerHTML = '<strong> Quarta-Feira </strong>';
+
+        break
+
+        case '4':
+        mostrar.innerHTML = '<strong> Quinta-Feira </strong>';
+
+        break
+
+        case '5':
+        mostrar.innerHTML = '<strong> Sexta-Feira </strong>';
+
+        break
+
+
+        case '6':
+        mostrar.innerHTML = '<strong> Sábado </strong>';
+
+        break
+
+    }
     
 }
