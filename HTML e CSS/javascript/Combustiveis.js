aa247e44fedcb1ad3fbf1c5ece99e7c405681c53
RuanRCA <assuncaoruan7@gmail.com>
2025-03-20 12:19:43 -0300

    divinput

--- HTML e CSS/javascript/Combustiveis.js
+++ HTML e CSS/javascript/Combustiveis.js
@@ -2,25 +2,31 @@ function calcular (){
     // estou apresentando o valor digitado pelo usuário
     let preco = document.getElementById('preco');
     let valor = Number(preco.value);
-    //estou pegando apresentado o select
+    //estou pegando e apresentado o select
     let select = document.getElementById('select');
-    // estou buscando o valor informado para fazer os calculos. obs o select é 1 array
+    // estou buscando o valor informado para fazer os calculos a partir das 'option'. obs o select é 1 array(vetor)
     let valores = select.options [select.selectedIndex];
     // estou pegando o valor a partir do value informado no select do html
     let porcetagem = valores.value;
+    // conta para calcular o valor da gasolina
     let calcgasolina = (valor*porcetagem);
-    let calcetanol = 1/(valor*porcetagem);
+    const total = calcgasolina.toFixed(2)
+    // conta para calcular o valor do etanol
+    let calcetanol = 1/(valor*porcetagem)*100;
     let resultado = document.getElementById('resultado');
+     // este código está pegando os calculos e fazendo a conversão para dinheiro
+    const totalgasolina = calcgasolina.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    const totaletanol = calcetanol.toLocaleString('pt-BR' ,{style: 'currency' , currency: 'BRL' } );
 
     if(document.getElementById('gasolina').checked){
-      resultado.innerText = `O valor ficará de R$ ${calcgasolina}`;
+      resultado.innerText = `Abasteça com Etanol se o seu  preço for menor  ou igual a : ${totalgasolina}`;
     }
 
     else if (document.getElementById('etanol').checked){
-        resultado.innerText = `O valor ficará de R$ ${calcetanol}`;
+        resultado.innerText = `Abasteça com gasolina se o seu o seu preço for menor ou igual a :  ${totaletanol}`;
     }
 
     else{
-        resultado.innerText = "nenhum valor encontrado";
+        resultado.innerText = "Nenhum valor encontrado";
     }
 }
