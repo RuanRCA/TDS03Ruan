9fab12183c2c9cf954455d65272eb40b3e680add
RuanRCA <assuncaoruan7@gmail.com>
2025-04-16 00:04:14 -0300

    .

---
+++ HTML e CSS/javascript/mercedez.js
@@ -0,0 +1,51 @@
+class MobileNavbar{
+    constructor(mobileMenu , nav , navLinks){
+        this.mobileMenu= document.querySelector(mobileMenu);
+        this.nav = document.querySelector(nav);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick= this.handleClick.bind(this);
+
+    }
+
+    animateLinks(){
+    this.navLinks.forEach((link,index)=>{
+        console.log(index);
+
+        link.style.animation
+        ?(link.style.animation="")
+        :(link.style.animation = `navLinkFade 0.5 ease forwards 0.3s`);
+
+    });
+    }
+
+
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addClickEventListener("click" , this.handleClick );
+    
+    }
+
+    init(){
+
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+
+        return this;
+
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavbar.init();
