ae981a45de8ec7a1c9960dd4ae0e7283a8b089e6
RuanRCA <assuncaoruan7@gmail.com>
2025-04-20 21:13:42 -0300

    .contuacao

--- HTML e CSS/javascript/mercedez.js
+++ HTML e CSS/javascript/mercedez.js
@@ -49,3 +49,20 @@ const mobileNavbar = new MobileNavbar(
     ".nav-list li",
 );
 mobileNavbar.init();
+
+
+let count = 1 ;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    
+}, 5000);
+
+function nextImage(){
+ count++;
+ if(count>4){
+    count =1;
+ }
+
+ document.getElementById("radio" + count).checked=true;
+}
