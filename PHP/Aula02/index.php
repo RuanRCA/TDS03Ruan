b5d420ec9f149671af79e41097797a6c93c33d46
RuanRCA <assuncaoruan7@gmail.com>
2025-04-23 11:33:51 -0300

    .

---
+++ PHP/Aula02/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>url</title>
+</head>
+<body>
+
+<?php
+ 
+ $N1 = $_GET["a"];
+ $N2 = $_GET["b"];
+
+ $soma = $N1 + $N2;
+
+ echo $soma;
+
+
+?>
+    
+</body>
+</html>
